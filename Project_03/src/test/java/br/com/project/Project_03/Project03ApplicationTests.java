9033576479d7dbd0ade8a08597be981ba51faa08
Gus303 <gurocha05@gmail.com>
2023-05-03 09:50:45 -0300

    TestProject

---
+++ Project_03/src/test/java/br/com/project/Project_03/Project03ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.project.Project_03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project03ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
